28a976a3c0869b795bf1aa83e2db36a510dfca9c
SimaoKovin <44097298+SimaoKovin@users.noreply.github.com>
2019-04-27 17:47:00 +0200

    Update main.js
    Added Alarm Clock code

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let hourDiv = document.getElementById('hourHand');
 let minuteDiv = document.getElementById('minuteHand');
 let secondDiv = document.getElementById('secondHand');
 
- var clockOperate = () => {   //arrow function to meet the ES6 requirement
+var clockOperate = () => {   //arrow function to meet the ES6 requirement
   let date = new Date();
 
   let hours = date.getHours();
@@ -10,7 +10,7 @@ let secondDiv = document.getElementById('secondHand');
   let seconds = date.getSeconds();
 
   let hourDeg = (hours + minutes / 60 + seconds / 3600) * 30;
-  let minDeg = (minutes + (seconds / 60))* 6 ;
+  let minDeg = (minutes + (seconds / 60)) * 6;
   let secDeg = seconds * 6;
 
   hourDiv.style.transform = "rotate(" + hourDeg + "deg)";
@@ -23,3 +23,55 @@ let secondDiv = document.getElementById('secondHand');
 setInterval(function () {
   clockOperate();
 }, 1000)
+
+
+// Alarm
+
+
+var alarmSound = new Audio();
+alarmSound.src = 'GOTalarm.mp3';
+var alarmTimer;
+
+function setAlarm(button) {
+  var ms = document.getElementById('alarmTime').valueAsNumber;
+  if (isNaN(ms)) {
+    alert('By the gods, that is no date!')
+    return;
+  }
+
+  var alarm = new Date(ms);
+  var alarmTime = new Date(alarm.getUTCFullYear(), alarm.getUTCMonth(), alarm.getUTCDate(), alarm.getUTCHours(), alarm.getUTCMinutes(), alarm.getUTCSeconds());
+
+  var timeDifference = alarmTime.getTime() - (new Date()).getTime();
+
+  if (timeDifference < 0) {
+    alert('only Bran Stark may visit times passed');
+    return;
+  }
+  alarmTimer = setTimeout(startAlarm, timeDifference);
+  button.innerText = 'Cancel Alarm';
+  button.setAttribute('onclick', 'cancelAlarm(this);');
+};
+
+function cancelAlarm(button) {
+  clearTimeout(alarmTimer);
+  button.innerText = 'Set Alarm';
+  button.setAttribute('onclick', 'setAlarm(this);')
+};
+
+function startAlarm() {
+  alarmSound.play();
+  document.getElementById('alarmAddOptions').style.display = '';
+};
+
+function stopAlarm() {
+  alarmSound.pause();
+  alarmSound.currentTime = 0;
+  document.getElementById('alarmAddOptions').style.display = 'none';
+  cancelAlarm(document.getElementById('alarmButton'));
+};
+
+function snooze() {
+  stopAlarm();
+  alarmTimer = setTimeout(startAlarm, 36000);
+};
